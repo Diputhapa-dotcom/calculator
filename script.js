9589c22a58a4667b568df6e03afad001a7791dc6
Diputhapa-dotcom <magardeepu53@gmail.com>
2026-05-19 20:41:40 +0545

    fetchin-dataW

--- script.js
+++ script.js
@@ -55,3 +55,29 @@ function decimal(){
 
     document.querySelector(".output").value = expression;
 }
+
+
+async function dataFetching(){
+    let store=await fetch("https://fakestoreapi.com/products");
+    let parent=document.getElementById("parent")
+  if(store.status==200){
+    let data= await store.json();
+    console.log(data)
+    data.forEach((items)=>{
+        let div=document.createElement("div");
+        let img=document.createElement("img");
+        let h3=document.createElement("h3");
+
+        img.src=items.image;
+        h3.innerHTML=items.title;
+        
+        div.appendChild(img);
+        div.appendChild(h3);
+        parent.appendChild(div);
+        
+
+    });
+  }
+
+}
+dataFetching();
